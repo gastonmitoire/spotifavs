9cb5f8728b70fdaaf430b919efb94967824da949
Gastón Mitoire <gastonmm@gmail.com>
2024-08-21 03:54:31 -0300

    feat: aside navigation init

--- app/_dashboard/_components/Dashboard.tsx
+++ app/_dashboard/_components/Dashboard.tsx
@@ -1,32 +1,68 @@
-import { Avatar, Divider, User } from "@nextui-org/react";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, ReactNode } from "react";
+import { Avatar, Divider as NextUIDivider, User } from "@nextui-org/react";
+
+import {
+  Home as HomeIcon,
+  Profile2User as Profile2UserIcon,
+  Music as MusicIcon,
+} from "iconsax-react";
 
 interface ListContainerProps extends PropsWithChildren {
   className?: string;
 }
 
+interface NavigationItemProps extends PropsWithChildren {
+  iconName: "home" | "artists" | "songs";
+}
+
+const iconMap: Record<NavigationItemProps["iconName"], React.ComponentType> = {
+  home: HomeIcon,
+  artists: Profile2UserIcon,
+  songs: MusicIcon,
+};
+
+const borderColor = "border-opacity-10 border-white";
+
 const ListContainer = ({ children, className }: ListContainerProps) => {
-  const baseStyles = "border-l-1 border-opacity-20 border-shadow h-full";
+  const baseStyles = borderColor + " " + "h-full";
 
   return <div className={baseStyles + " " + className}>{children}</div>;
 };
 
-const NavigationItem = ({ children }: PropsWithChildren) => {
+const NavigationItem = ({ children, iconName }: NavigationItemProps) => {
+  const IconComponent = iconMap[iconName];
   return (
-    <li className="font-thin text-2xl py-3 pl-3 cursor-pointer">{children}</li>
+    <li
+      className={
+        borderColor +
+        " " +
+        "font-thin text-2xl pl-3 cursor-pointer pr-3 border-l-1"
+      }
+    >
+      <div className="flex items-center gap-5 py-3">
+        <IconComponent />
+        {children}
+      </div>
+    </li>
   );
 };
 
-const NavigationList = () => {
+const NavigationList = ({
+  borderPlacement,
+}: {
+  borderPlacement: "top" | "bottom";
+}) => {
   return (
-    <ul className="space-y-1.5">
-      <Divider />
-      <NavigationItem>Inicio</NavigationItem>
-      <Divider />
-      <NavigationItem>Artistas</NavigationItem>
-      <Divider />
-      <NavigationItem>Canciones</NavigationItem>
-      <Divider />
+    <ul
+      className={
+        borderPlacement === "top"
+          ? "[&>li]:border-t-1 first:[&>li]:rounded-tl-xl"
+          : "[&>li]:border-b-1 last:[&>li]:rounded-bl-xl"
+      }
+    >
+      <NavigationItem iconName="home">Inicio</NavigationItem>
+      <NavigationItem iconName="artists">Artistas</NavigationItem>
+      <NavigationItem iconName="songs">Canciones</NavigationItem>
     </ul>
   );
 };
@@ -35,20 +71,27 @@ export const Dashboard = () => {
   return (
     <div className="flex h-full">
       <section className="h-full flex-1">a</section>
-      <section className="flex flex-col justify-center items-end gap-5">
+
+      <aside className="flex flex-col justify-center items-end">
         <ListContainer className="justify-end flex flex-col">
-          <NavigationList />
+          <NavigationList borderPlacement="top" />
         </ListContainer>
-        <div className="p-1.5 border-l-2 rounded-full grid place-items-center mr-12 border-opacity-20 border-shadow">
+        <div
+          className={
+            borderColor +
+            " " +
+            "p-1.5 border-l-2 rounded-full grid place-items-center mr-24"
+          }
+        >
           <Avatar
             src="https://i.pravatar.cc/150?u=a04258114e29026708c"
             className="w-32 h-32 text-large"
           />
         </div>
         <ListContainer>
-          <NavigationList />
+          <NavigationList borderPlacement="bottom" />
         </ListContainer>
-      </section>
+      </aside>
     </div>
   );
 };
